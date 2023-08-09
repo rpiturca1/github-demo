6a481b9ef9f16c064eb023ef78a884a605a06dc7
rpiturca1 <rpiturca@deloittece.com>
2023-08-09 12:32:48 +0300

    Strings

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import jdk.nashorn.internal.parser.JSONParser;
+import sun.awt.geom.AreaOp;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.printf("byte minimum= %s, maximum= %s%n", Byte.MIN_VALUE, Byte.MAX_VALUE);
+//        System.out.printf("short minimum= %s, maximum= %s%n", Short.MIN_VALUE, Short.MAX_VALUE);
+//        System.out.printf("int minimum= %s, maximum= %s%n", Integer.MIN_VALUE, Integer.MAX_VALUE);
+//        System.out.printf("long minimum= %s, maximum= %s%n", Long.MIN_VALUE, Long.MAX_VALUE);
+//        System.out.printf("float minimum= %s, maximum= %s%n", Float.MIN_VALUE, Float.MAX_VALUE);
+//        System.out.printf("double minimum= %s, maximum= %s%n", Double.MIN_VALUE, Double.MAX_VALUE);
+
+//        int answer = 7 + 3 * 4;
+//        System.out.println(answer);
+
+//        int x = 3;
+//        int y = x++;
+//        System.out.printf("Using x++, x is %s and y is %s.%n", x, y);
+
+        String courseName = "Learn Java for Beginners";
+        String message = "Welcome to ";
+//        concatenate
+//        String fullMessage = message + courseName;
+//        System.out.println(fullMessage);
+//
+//        //use string format
+//        String fullMessage2 = String.format("%s%s", message, courseName);
+//        System.out.println(fullMessage2);
+
+//        //use printf
+        System.out.printf("Hello, and %sthe %s.%n", message, courseName);
+        for (int i = 0; i < 37; i++) {
+            char character = courseName.charAt(i);
+            System.out.println(character);
+//        }
+        }
+    }
+}

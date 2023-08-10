4a7c755393f6fbe3287d6cd9bb6e85acaf207427
rpiturca1 <rpiturca@deloittece.com>
2023-08-10 17:16:09 +0300

    Logic exercises

---
+++ src/main/java/JavaStringExercises/IfElseExercise.java
@@ -0,0 +1,17 @@
+package JavaStringExercises;
+
+public class IfElseExercise {
+
+    public static class TestClass{public static void main(String[] args)
+
+    {
+        Object obj1 = new Object();
+
+        Object obj2 = obj1;
+
+ if( obj1.equals(obj2) ) System.out.println("true");
+
+ else
+
+        System.out.println("false");} }
+}

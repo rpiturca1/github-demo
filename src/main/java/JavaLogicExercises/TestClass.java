e568742dd24059e95f35073895db6bf60dce734c
rpiturca1 <rpiturca@deloittece.com>
2023-08-11 14:02:51 +0300

    test class

---
+++ src/main/java/JavaLogicExercises/TestClass.java
@@ -0,0 +1,7 @@
+public class TestClass {
+    public static void main(String[] args) throws Exception { boolean flag = true;
+        if (flag){
+            System.out.println("true");}
+
+            else {System.out.println("false"); }
+    } }

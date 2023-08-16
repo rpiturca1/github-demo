f9869043bb6b4fd6e5e444bfe0f3d09cc506dd79
rpiturca1 <rpiturca@deloittece.com>
2023-08-16 16:04:02 +0300

    test class ex

---
+++ src/main/java/JavaLogicExercises/Loops.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming;
+
+public  class Loops {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 9; i++) {
+            System.out.printf("row %d ", i);
+            for ( int j = 0; j <= 9; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
--- src/main/java/JavaLogicExercises/TestClass.java
+++ src/main/java/JavaLogicExercises/TestClass.java
@@ -1,7 +1,17 @@
-public class TestClass {
-    public static void main(String[] args) throws Exception { boolean flag = true;
-        if (flag){
-            System.out.println("true");}
+//public class TestClass {
+//    public static void main(String[] args) throws Exception { boolean flag = true;
+//        if (flag){
+//            System.out.println("true");}
+//
+//            else {System.out.println("false"); }
+//    } }
 
-            else {System.out.println("false"); }
-    } }
+
+public class TestClass {
+    //define tester method here
+    public boolean tester(){
+        return false;
+    }
+    public static void main(String[] args) throws Exception { TestClass tc = new TestClass();
+        while(tc.tester()){
+            System.out.println("running..."); } } }

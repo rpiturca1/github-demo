9b28fa1574d46929113dc9bef2a752483b7f3742
rpiturca1 <rpiturca@deloittece.com>
2023-08-17 17:51:27 +0300

    firstLast6

---
+++ src/main/java/JavaArrayExercises/firstLast6.java
@@ -0,0 +1,19 @@
+package JavaArrayExercises;
+
+public class firstLast6 {
+
+    public static void main(String[] args) {
+
+        System.out.println(firstLast6([1, 2, 6]));
+
+    }
+
+    public static boolean firstLast6(int[] nums) {
+
+        if ( nums[0] == 6 || nums[nums.length - 1] == 6)
+            return true;
+        else
+            return false;
+
+    }
+}

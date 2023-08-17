18369c753fb5c8fc5ed09b646f83d045c61e8b81
rpiturca1 <rpiturca@deloittece.com>
2023-08-17 16:19:23 +0300

    arrays

---
+++ src/main/java/JavaArrayExercises/test2.java
@@ -0,0 +1,13 @@
+//package JavaArrayExercises;
+//
+//public class test2 {
+//
+//    public static void main (String[] arg)
+//    {
+//
+//        int sum = 1+2;
+//        String name = "Ramona";
+//
+//System.out.println("My name is " + name + " and I have " + sum + " years old");
+//    }
+//}
--- src/main/java/JavaLogicExercises/TestClass.java
+++ src/main/java/JavaLogicExercises/TestClass.java
@@ -1,17 +1,42 @@
-//public class TestClass {
-//    public static void main(String[] args) throws Exception { boolean flag = true;
-//        if (flag){
-//            System.out.println("true");}
+////public class TestClass {
+////    public static void main(String[] args) throws Exception { boolean flag = true;
+////        if (flag){
+////            System.out.println("true");}
+////
+////            else {System.out.println("false"); }
+////    } }
 //
-//            else {System.out.println("false"); }
-//    } }
-
-
-public class TestClass {
-    //define tester method here
-    public boolean tester(){
-        return false;
-    }
-    public static void main(String[] args) throws Exception { TestClass tc = new TestClass();
-        while(tc.tester()){
-            System.out.println("running..."); } } }
+//
+////public class TestClass {
+////    //define tester method here
+////    public boolean tester(){
+////        return false;
+////    }
+////    public static void main(String[] args) throws Exception { TestClass tc = new TestClass();
+////        while(tc.tester()){
+////            System.out.println("running..."); } } }
+//
+//package academy.learnprogramming;
+//
+//import java.util.Scanner;
+//
+//public class TestClass {
+//
+//    public static void main(String[] args) {
+//
+//        var scanner = new Scanner(System.in);
+//
+//        System.out.println(Doctor.intro());
+//
+//        var userInput = "";
+//
+//        do {
+//
+//            userInput = scanner.nextLine();
+//            String response = Doctor.response(userInput);
+//            System.out.println(response);
+//        } while (!userInput.equalsIgnoreCase("quit"));
+//
+//        scanner.close();
+//    }
+//}
